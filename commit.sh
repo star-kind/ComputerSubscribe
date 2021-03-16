d2908e017161333744388441466b6074371985d6
AllStarGH <duernna@163.com>
2021-03-17 04:28:41 +0800

    First Submit

--- commit.sh
+++ commit.sh
@@ -18,7 +18,7 @@ git remote -v
 
 git add .
 
-git commit -m 'first committing to here'
+git commit -m 'second committing to here'
 
 # error: 无法推送一些引用到 'https://github.com/AllStarGH/independ-react-engineering.git'
 # 提示：更新被拒绝，因为您当前分支的最新提交落后于其对应的远程分支。
