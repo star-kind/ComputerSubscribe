0504a6bd723fe3b45879e9e80f5f746061b14a50
AllStarGH <duernna@163.com>
2021-03-17 06:03:33 +0800

    second committing to here

--- git-commit.sh
+++ git-commit.sh
@@ -25,11 +25,4 @@ git add .
 
 git commit -m 'second committing to here'
 
-# error: 无法推送一些引用到 'https://github.com/AllStarGH/independ-react-engineering.git'
-# 提示：更新被拒绝，因为您当前分支的最新提交落后于其对应的远程分支。
-# resolved:
-# git pull origin master --allow-unrelated-histories
-
-git pull origin master
-
-git push origin master
+git push -u origin main
